5f89da07b0bb7f727874f31128e9449a5af535b3
EmersonJr <contato.juniormorales@gmail.com>
2024-05-10 16:23:41 -0300

    Atualização

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,9 +1,16 @@
 package controller;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 public class LoginController {
 	
@@ -13,11 +20,28 @@ public class LoginController {
 	@FXML
 	private PasswordField senha;
 	
+	public Stage primaryStage;
+	
 	public void entrar() {
 		System.out.println("Tentativa de login...");
 		if(email.getText().equals("admin@gmail.com")
 				&& senha.getText().equals("admin")) {
-			System.out.println("Realizado com sucesso");
+			
+			try {
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/DashboardSubaru.fxml"));
+				
+				StackPane root = loader.load();
+				
+				Scene scene = new Scene(root, 922, 526);
+				
+				Stage currentStage = (Stage) email.getScene().getWindow();
+				currentStage.setScene(scene);
+				currentStage.setTitle("DashboardSubaru");
+				currentStage.show();				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
 		}else {
 			mensagemDeErro();
 		}
